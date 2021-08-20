f65f8d83d4566217b7f8d4675a1ceada3ed2c2db
Reza Ghahremani <developer.ghahremani@gmail.com>
2021-08-20 14:12:07 +0430

    hide scroll on flat lists

--- src/component/general/IFlatlist.js
+++ src/component/general/IFlatlist.js
@@ -2,7 +2,13 @@ import React from "react";
 import { StyleSheet, Text, View, FlatList } from "react-native";
 
 const IFlatlist = ({ ...prp }) => {
-  return <FlatList keyExtractor={(item, index) => `${index}`} {...prp} />;
+  return (
+    <FlatList
+      showsVerticalScrollIndicator={false}
+      keyExtractor={(item, index) => `${index}`}
+      {...prp}
+    />
+  );
 };
 
 export default IFlatlist;
